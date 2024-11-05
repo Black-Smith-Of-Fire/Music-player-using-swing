b5548c8e3c5c0a30738cd0a1655a4c90ea0a7b65
Black_Smith_Of_Fire <blacksmithoffire@gmail.com>
2024-11-05 22:10:59 +0530

    Using the AdvancedPlayer class

--- src/PlayerMp3.java
+++ src/PlayerMp3.java
@@ -1,27 +1,41 @@
 import javazoom.jl.decoder.JavaLayerException;
 import javazoom.jl.player.Player;
+import javazoom.jl.player.advanced.AdvancedPlayer;
+import javazoom.jl.player.advanced.PlaybackEvent;
+import javazoom.jl.player.advanced.PlaybackListener;
 
 import java.util.Scanner;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 
 public class PlayerMp3 {
-    public static void main(String[] args) {
+    private int pausedOnFrame = 0;
+
+    public void PlayerMp() {
         Scanner scanner = new Scanner(System.in);
         try {
             FileInputStream fileInputStream = new FileInputStream("src/tootttooottoo.mp3");
-            Player player = new Player(fileInputStream);
-            player.play();
-            if (scanner.next().equals("k")) {
+            AdvancedPlayer player = new AdvancedPlayer(fileInputStream);
+            String s = scanner.nextLine();
 
+            if (s.equals("k")){
+            player.setPlayBackListener(new PlaybackListener() {
+                @Override
+                public void playbackFinished(PlaybackEvent event) {
+                    pausedOnFrame = event.getFrame();
+                }
+            });
             }
-            System.out.println("Song is playing");
-        }
-        catch (FileNotFoundException e) {
+            player.play(pausedOnFrame, Integer.MAX_VALUE);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (JavaLayerException e) {
             e.printStackTrace();
         }
-        catch (JavaLayerException e) {
-            e.printStackTrace();
-        }
+    }
+
+    public static void main(String[] args) {
+        PlayerMp3 mp = new PlayerMp3();
+        mp.PlayerMp();
     }
 }
