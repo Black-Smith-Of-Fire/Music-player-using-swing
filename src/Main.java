ae93b4aa159bfb8c9cbe7beb9939d4ca753d4a53
Black_Smith_Of_Fire <blacksmithoffire@gmail.com>
2024-11-05 21:01:35 +0530

    Main driver class

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import  javax.swing.*;
+import java.awt.*;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setSize(500,500);
+        frame.setTitle("Music Player");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
