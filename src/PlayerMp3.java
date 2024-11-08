70ebf5d28790534e40477988655b3fc6c043c571
Black_Smith_Of_Fire <blacksmithoffire@gmail.com>
2024-11-08 23:52:16 +0530

    Multithreading the class

--- src/PlayerMp3.java
+++ src/PlayerMp3.java
@@ -8,25 +8,23 @@ import java.util.Scanner;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 
-public class PlayerMp3 {
-    private int pausedOnFrame = 0;
+public class PlayerMp3 implements Runnable {
+
+    private boolean exit; // if true , it will cause the action within the thread to stop
+    Thread thread ;
+
+    PlayerMp3(){
+        thread = new Thread(this);
+        exit = false;
+        thread.start();
+    }
 
     public void PlayerMp() {
-        Scanner scanner = new Scanner(System.in);
         try {
             FileInputStream fileInputStream = new FileInputStream("src/tootttooottoo.mp3");
             AdvancedPlayer player = new AdvancedPlayer(fileInputStream);
-            String s = scanner.nextLine();
+            player.play();
 
-            if (s.equals("k")){
-            player.setPlayBackListener(new PlaybackListener() {
-                @Override
-                public void playbackFinished(PlaybackEvent event) {
-                    pausedOnFrame = event.getFrame();
-                }
-            });
-            }
-            player.play(pausedOnFrame, Integer.MAX_VALUE);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (JavaLayerException e) {
@@ -34,8 +32,23 @@ public class PlayerMp3 {
         }
     }
 
+    @Override
+    public void run() {
+        while(!exit){
+            PlayerMp();
+        }
+    }
+
+    public void stop(){ // use this method to stop the thread
+        exit = true;
+    }
+
     public static void main(String[] args) {
         PlayerMp3 mp = new PlayerMp3();
-        mp.PlayerMp();
+        Scanner scanner = new Scanner(System.in);
+        if (scanner.nextLine().equals("s")){
+            mp.stop();
+        }
     }
+
 }
